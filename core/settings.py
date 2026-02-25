3864a04dc6d7f65844877a032c43486f766090fe
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-02-25 11:25:06 +0100

    Se modifica el settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,12 +1,21 @@
 from pathlib import Path
+import os
 
+import environ
+
+env = environ.Env() 
+environ.Env.read_env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-$5v21v9z4^6kcyr6ys4t$uvs=wr(y)k2&cz6(m84e8lk!tu8#k'
+#SECRET_KEY = 'django-insecure-$5v21v9z4^6kcyr6ys4t$uvs=wr(y)k2&cz6(m84e8lk!tu8#k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -18,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -35,7 +46,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

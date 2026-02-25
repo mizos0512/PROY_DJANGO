20ea33adaccd7195d1917f08875d636eda7434d3
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-02-25 11:52:52 +0100

    Se crea el archivo urls.py de blog

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import BlogListView, BlogCreateView, BlogDetailView
+
+app_name="blog"
+
+
+urlpatterns = [
+  path('', BlogListView.as_view(), name='home'),
+  path('create/', BlogCreateView.as_view(), name='create'),
+  path('<int:pk>/', BlogDetailView.as_view(), name='detail')
+  
+  ]
+

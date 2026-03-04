005703df8ece9c9093efc25ad22e8f10a063be03
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-03-04 12:26:23 +0100

    Se modifica 'blog/urls.py'

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ app_name="blog"
 
 urlpatterns = [
   path('', BlogListView.as_view(), name='home'),
+  
   path('create/', BlogCreateView.as_view(), name='create'),
   path('<int:pk>/', BlogDetailView.as_view(), name='detail')
   

58952da1ed4fc25814e3965161daa4187ec7271d
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-04-03 20:19:18 +0200

    Se modifico la url de blog

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
   path('', BlogListView.as_view(), name='home'),
   
   path('create/', BlogCreateView.as_view(), name='create'),
+  
   path('<int:pk>/', BlogDetailView.as_view(), name='detail')
   
   ]
